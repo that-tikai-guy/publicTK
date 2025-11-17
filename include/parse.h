587426146b066f8d21f2359565d50089caa3de2e
that-tikai-guy <kaisertim@outlook.de>
2025-11-17 15:54:07 +0100

    more fix

--- include/parse.h
+++ include/parse.h
@@ -2,6 +2,8 @@
 #define PARSE_H
 
 #define HEADER_MAGIC 0x4c4c4144
+#define NAME_LEN 256
+#define ADDRESS_LEN 256
 
 struct dbheader_t {
 	unsigned int magic;
@@ -11,16 +13,13 @@ struct dbheader_t {
 };
 
 struct employee_t {
-	char name[256];
-	char address[256];
+	char name[NAME_LEN];
+	char address[ADDRESS_LEN];
 	unsigned int hours;
 };
 
 int create_db_header(int fd, struct dbheader_t **headerOut);
 int validate_db_header(int fd, struct dbheader_t **headerOut);
-int read_employees(int fd, struct dbheader_t *, struct employee_t **employeesOut);
 int output_file(int fd, struct dbheader_t *, struct employee_t *employees);
-void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees);
-int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring);
 
 #endif
