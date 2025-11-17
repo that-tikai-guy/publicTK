d65d117e9900b1daa7551afff52e8888d3f9f77d
that-tikai-guy <kaisertim@outlook.de>
2025-11-17 15:40:54 +0100

    adds database headers

---
+++ database/src/main.c
@@ -0,0 +1,83 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <getopt.h>
+#include <stdlib.h>
+
+#include "common.h"
+#include "file.h"
+#include "parse.h"
+
+void print_usage(char *argv[]) {
+    printf("Usage: %s -n -f <database file>\n", argv[0]);
+    printf("\t -n - create new database file\n");
+    printf("\t -f - (required) path to database file\n");
+    return;
+}
+
+int main(int argc, char *argv[]) {
+    char *filepath = NULL;
+    char *portarg = NULL;
+    unsigned short port = 0;
+    bool newfile = false;
+    bool list = false;
+    int c;
+    int dbfd = -1;
+    struct dbheader_t *dbhdr = NULL;
+
+    while ((c = getopt(argc, argv, "nf:a:l")) != -1) {
+        switch (c) {
+            case 'n':
+                newfile = true;
+                break;
+            case 'f':
+                filepath = optarg;
+                break;
+            case 'p':
+                portarg = optarg;
+                break;
+            case 'l':
+                list = true;
+                break;
+            case '?':
+                printf("Unknown option -%c\n", c);
+                break;
+            default:
+                return -1;
+        }
+    }
+
+    if (filepath == NULL) {
+        printf("Filepath is a required argument!\n");
+        print_usage(argv);
+        return 0;
+    }
+
+    if (newfile) {
+        dbfd = create_db_file(filepath);
+        if (dbfd == STATUS_ERROR) {
+            printf("Unable to create database file!\n");
+            return -1;
+        }
+
+        if (create_db_header(dbfd, &dbhdr) != STATUS_SUCCESS) {
+            printf("Unable to create database header!\n");
+            return -1;
+        }
+
+    } else {
+        dbfd = open_db_file(filepath);
+        if (dbfd == STATUS_ERROR) {
+            printf("Unable to open database file!\n");
+            return -1;
+        }
+
+        if (validate_db_header(dbfd, &dbhdr) != STATUS_SUCCESS) {
+            printf("Unable to validate database header!\n");
+            return -1;
+        }
+    }
+
+    //output_file(dbfd, dbhdr);
+
+    return 0;
+}
---
+++ database/src/parse.c
@@ -0,0 +1,108 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <string.h>
+
+#include "common.h"
+#include "parse.h"
+
+void list_employees(struct dbheader_t *dbhdr, struct employee_t *employees) {
+
+}
+
+int add_employee(struct dbheader_t *dbhdr, struct employee_t *employees, char *addstring) {
+
+}
+
+int read_employees(int fd, struct dbheader_t *dbhdr, struct employee_t **employeesOut) {
+
+}
+
+int output_file(int fd, struct dbheader_t *dbhdr, struct employee_t *employees) {
+    if (fd < 0) {
+        printf("Bad FD input!\n");
+        return STATUS_ERROR;
+    }
+
+    dbhdr->magic    = htonl(dbhdr->magic);
+    dbhdr->version  = htons(dbhdr->version);
+    dbhdr->count    = htons(dbhdr->count);
+    dbhdr->filesize = htonl(dbhdr->filesize);
+
+    lseek(fd, 0, SEEK_SET);
+
+    write(fd, dbhdr, sizeof(struct dbheader_t));
+
+    return STATUS_SUCCESS;
+}
+
+int validate_db_header(int fd, struct dbheader_t **headerOut) {
+    if (fd < 0) {
+        printf("Bad FD input!\n");
+        return STATUS_ERROR;
+    }
+
+    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+    if (header == NULL) {
+        printf("MemAlloc failed creating database header!\n");
+        return STATUS_ERROR;
+    }
+
+    if (read(fd, header, sizeof(struct dbheader_t)) != sizeof(struct dbheader_t)) {
+        perror("read");
+        free(header);
+        return STATUS_ERROR;
+    }
+
+    header->magic    = ntohl(header->magic);
+    header->version  = ntohs(header->version);
+    header->count    = ntohs(header->count);
+    header->filesize = ntohl(header->filesize);
+
+    if (header->magic != HEADER_MAGIC) {
+        printf("Header magic corrupted!\n");
+        free(header);
+        return -1;
+    }
+
+    if (header->version != 1) {
+        printf("Header version corrupted!\n");
+        free(header);
+        return -1;
+    }
+
+    struct stat dbstat = {0};
+    fstat(fd, &dbstat);
+    if (header->filesize != dbstat.st_size) {
+        printf("Database corrupted!\n");
+        free(header);
+        return -1;
+    }
+
+    *headerOut = header;
+
+    return STATUS_SUCCESS;
+}
+
+int create_db_header(int fd, struct dbheader_t **headerOut) {
+    struct dbheader_t *header = calloc(1, sizeof(struct dbheader_t));
+    if (header == NULL) {
+        printf("MemAlloc failed creating database header!\n");
+        return STATUS_ERROR;
+    }
+
+    header->version  = 0x1;
+    header->count    = 0;
+    header->magic    = HEADER_MAGIC;
+    header->filesize = sizeof(struct dbheader_t);
+
+    *headerOut = header;
+
+    return STATUS_SUCCESS;
+}
+
+
